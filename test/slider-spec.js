25b613577c84936e6b9296ef8712c6205f400508
Patrick Mast <mast@ubilabs.net>
2015-07-02 17:43:56 +0200

    initial commit, setting up environment, testing environment

---
+++ test/slider-spec.js
@@ -0,0 +1,10 @@
+// slider component
+// - should increase by one
+
+import assert from 'assert';
+
+describe('env', function () {
+  it('is working', function () {
+    assert.equal(1, 1);
+  });
+});
