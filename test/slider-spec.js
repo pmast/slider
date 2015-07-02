72a979647d33a368c94ea98619eb3e3788f22c81
Patrick Mast <mast@ubilabs.net>
2015-07-02 18:19:05 +0200

    slider is evolving: increase, decrease, boundaries

--- test/slider-spec.js
+++ test/slider-spec.js
@@ -1,6 +1,14 @@
 // slider component
 // - should have a current value
 // - should be increasable
+// - should have a maximum value
+// - should have a minimum value
+// - should take a start value
+// - should take a min value
+// - should take a max value
+// - should not increase beyond max value
+// - should be decreasable
+// - should not decrease beyond minimum value
 
 import assert from 'assert';
 
@@ -11,25 +19,74 @@ describe('env', function () {
 });
 
 class Slider {
-  constructor() {
-    this.value = 0;
+  constructor(initialValue = 0, minValue = 0, maxValue = 10) {
+    this.value = initialValue;
+    this.maxValue = maxValue;
+    this.minValue = minValue;
   }
 
-  increase() {
-    this.value++;
+  increaseValue() {
+    this.value = Math.min(this.value + 1, this.maxValue);
+  }
+
+  decreaseValue() {
+    this.value = Math.max(this.value - 1, this.minValue);
   }
 }
 
+let maxValue = 10;
+let minValue = 0;
+let slider = new Slider(5, minValue, maxValue);
+
 describe('slider', function () {
   it('should have a current value', function () {
-    let slider = new Slider();
     assert.ok('value' in slider);
   });
 
+  it('should take a start value', function () {
+    assert.equal(slider.value, 5);
+  });
+
   it('should increase by one', function () {
-    let slider = new Slider();
-    let oldValue = slider.value;
-    slider.increase();
+    let oldValue = 5;
+    slider.value = oldValue;
+    slider.increaseValue();
     assert.equal(slider.value, oldValue + 1);
+  });
+
+  it('should have a max value', function () {
+    assert.ok('maxValue' in slider);
+  });
+
+  it('should have a min value', function () {
+    assert.ok('minValue' in slider);
+  });
+
+
+  it('should take a min value', function () {
+    assert.equal(slider.minValue, 0);
+  });
+
+  it('should take a max value', function () {
+    assert.equal(slider.maxValue, 10);
+  });
+
+  it('should not increase beyond the max value', function () {
+    slider.value = slider.maxValue;
+    slider.increaseValue();
+    assert.equal(slider.value, maxValue);
+  });
+
+  it('should be decreaseable', function () {
+    let oldValue = 5;
+    slider.value = oldValue;
+    slider.decreaseValue();
+    assert.equal(slider.value, oldValue - 1);
+  });
+
+  it('should not increase beyond the max value', function () {
+    slider.value = slider.minValue;
+    slider.decreaseValue();
+    assert.equal(slider.value, minValue);
   });
 });
