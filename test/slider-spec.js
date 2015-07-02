e47972d0e56511a88a79451bcb591f39b6b5d48d
Patrick Mast <mast@ubilabs.net>
2015-07-02 17:53:58 +0200

    increase by one

--- test/slider-spec.js
+++ test/slider-spec.js
@@ -1,10 +1,35 @@
 // slider component
-// - should increase by one
+// - should have a current value
+// - should be increasable
 
 import assert from 'assert';
 
 describe('env', function () {
   it('is working', function () {
     assert.equal(1, 1);
+  });
+});
+
+class Slider {
+  constructor() {
+    this.value = 0;
+  }
+
+  increase() {
+    this.value++;
+  }
+}
+
+describe('slider', function () {
+  it('should have a current value', function () {
+    let slider = new Slider();
+    assert.ok('value' in slider);
+  });
+
+  it('should increase by one', function () {
+    let slider = new Slider();
+    let oldValue = slider.value;
+    slider.increase();
+    assert.equal(slider.value, oldValue + 1);
   });
 });
